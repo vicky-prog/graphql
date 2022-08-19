14a0f466bcc2870617fe397c766797db8ec6468c
vicky-prog <johnvick281997@gmail.com>
2022-08-19 19:28:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const { ApolloServer, gql } = require('apollo-server');
+//3:g~~@EkB*u9Tmy
+const typeDefs = gql`
+ 
+  type Book {
+    title: String!
+    author: String
+  }
+
+ 
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+  const {
+    ApolloServerPluginLandingPageLocalDefault
+  } = require('apollo-server-core');
+  
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    /**
+     * What's up with this embed: true option?
+     * These are our recommended settings for using AS;
+     * they aren't the defaults in AS3 for backwards-compatibility reasons but
+     * will be the defaults in AS4. For production environments, use
+     * ApolloServerPluginLandingPageProductionDefault instead.
+    **/
+    plugins: [
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+  
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
